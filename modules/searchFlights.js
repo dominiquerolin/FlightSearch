9f9ad4366bbdd6e5465311966ca67f0619ace76c
dominiquerolin <rolin.dominique@gmail.com>
2015-02-28 15:39:05 +1000

    fix sort by date

--- modules/searchFlights.js
+++ modules/searchFlights.js
@@ -1,21 +1,25 @@
+/*
+ * Walks through the flight JSON to find matches
+ */
 module.exports = function(from, to, date, callback) {
 	var fs = require('fs'), moment = require('moment');
 	var flights = JSON.parse(fs.readFileSync('./public/javascripts/flights.json','utf-8'));
 	var filtered = [];
 	flights.forEach(function(f){
 		if(f.from == from && f.to == to && moment(f.departure).isSame(date, 'day')) {
+			// split date/time for easier display
 			f.departure_date = f.departure.split(' ')[0];
 			f.departure_time = f.departure.split(' ')[1];
 			f.arrival_date = f.arrival.split(' ')[0];
 			f.arrival_time = f.arrival.split(' ')[1];
-			f.duration = moment(f.arrival).diff(moment(f.departure), 'minutes');
 			filtered.push(f);
 		}
 	});
+	// order by date asc
 	filtered.sort(function(a,b) {
-		var aDate = new Date(a.date+' '+a.time);
-		var bDate = new Date(b.date+' '+b.time);
-		return aDate.getTime() > bDate.getTime() ? 1 : -1; 
+		var aDate = new Date(a.departure);
+		var bDate = new Date(b.departure);
+		return aDate > bDate; 
 	});
 	callback(null, filtered);
 }
